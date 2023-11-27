403c6b381028314bef7cdaa4ea7ba21eb88ec35d
Kaleighh Hyde <kaleighh@student.21-school.ru>
2023-11-27 23:36:29 +0700

    passwords

---
+++ ex00/Chat/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO
+    chat.user
+VALUES
+    ('Victory', 'treeeeeeee'),
+    ('Manarbeck', 'DeReVyAsHkA'),
+    ('Azamat', '1967-02-28'),
+    ('Annette', 'PINKYpink'),
+    ('Alex', 'Mumbai'),
+    ('Garfild', 'lasagna'),
+    ('Beethoven', 'GuFGuF');
--- ex00/Chat/src/main/resources/schema.sql
+++ ex00/Chat/src/main/resources/schema.sql
@@ -1,20 +1,28 @@
 DROP SCHEMA IF NOT EXISTS chat;
 CREATE SCHEMA IF EXISTS chat;
 
-CREATE TABLE chat.user (
-    id SERIAL PRIMARY KEY,
-    login ,
-    password
+CREATE TABLE IF NOT EXISTS chat.user (
+    id SERIAL PRIMARY KEY NOT NULL,
+    login VARCHAR(20) NOT NULL,
+    password VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE chat.chatroom (
+CREATE TABLE IF NOT EXISTS chat.chatroom (
     id SERIAL PRIMARY KEY,
-    login ,
-    password
+    name VARCHAR(20) NOT NULL,
+    owner INTEGER NOT NULL REFERENCES chat.user(id)
 );
 
-CREATE TABLE chat.meassage (
+CREATE TABLE IF NOT EXISTS chat.meassage (
     id SERIAL PRIMARY KEY,
-    login ,
-    password
+    author INTEGER NOT NULL REFERENCES chat.user(id) ,
+    room INTEGER NOT NULL REFERENCES chat.chatroom(id),
+    "text" TEXT NOT NULL,
+    date_time TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS chat.user_chat(
+    id SERIAL PRIMARY KEY,
+    user INTEGER NOT NULL REFERENCES chat.user(id) ,
+    room INTEGER NOT NULL REFERENCES chat.chatroom(id),
 );
